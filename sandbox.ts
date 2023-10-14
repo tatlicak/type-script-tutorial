59c08dbe1d59f7d6535ce3dbadca9d8a460f0361
Abdullah TATLICAK <tatlicak@gmail.com>
2023-10-14 07:44:09 +0300

    Lesson-4 is finished

--- sandbox.ts
+++ sandbox.ts
@@ -1,23 +1,58 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
 
-//type can not be changed
-//character = 20;
+let names = ['luigi','mario', 'yoshi'];
 
-character = 'luigi';
+//you can not do this
+//names.push(15);
 
-age = 40.0;
+let numbers = [1,2,3,4,5]
 
-isBlackBelt = true;
+//you can not do this again
+//numbers.push('bison');
 
-//type is checking in compile time for develpment not in runtime
-const circ = (diameter:number) => {
+let mixed = [1, 'a', 2, 'canada', 3, 'eskişehir']
 
-    return diameter * Math.PI
+//now you can do that
+mixed.push(5)
+mixed.push('otronto')
 
+//objects
+
+let person = {
+
+    name : "Abdullah",
+    surname : "TATLICAK",
+    title : "Web Developer",
+    age : 39,
+    skills : ['php','laravel','jquery']
 }
 
-console.log(circ(7.5));
+//not allowed
 
+//person.age = 'elli'
 
+//person = "Test"
+
+person.name = "rachel"
+
+person.age = 25
+
+//Allowed
+person = {
+
+    name : "Kerem",
+    surname : "YALÇIN",
+    title : "Mobile Developer",
+    age : 32,
+    skills : ['ios','flutter','android']
+}
+
+// Not Allowed
+
+person = {
+
+    name : "Kerem",
+    surname : "YALÇIN",
+    title : "Mobile Developer",
+    skills : ['ios','flutter','android']
+}
